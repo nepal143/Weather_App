fbb059c56e1fbcf3adb46994bbd7a9337deb721d
Nepal Singh <nepalsss007@gmail.com>
2023-06-24 22:22:13 +0530

    updating page 404

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const cityName = document.getElementById("cityName");
 const city_name = document.getElementById("city_name");
 const temperature = document.getElementById("temperature");
 const temp_status = document.getElementById("temp_status");
-const body = document.getElementsByClassName('body');
+const body = document.getElementById('body');
 
 const url =`api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1ed3c78650f1906854b9f37bd7565bd1`
 const submit = async (event)=>{
@@ -22,15 +22,18 @@ const submit = async (event)=>{
             console.log(weather1);
             if(weather1 == 'Clear'){
                 temp_status.innerHTML =  ' <i class="fa-sun fa" aria-hidden="true"></i>';
-                
+                body.style.backgroundImage = 'url(images/clear.jpg)';
             }
             else if(weather1 == 'Cloud'){
                 temp_status.innerHTML = '<i class="fa-cloud fa" aria-hidden="true"></i>';
+                body.style.backgroundImage = 'url(images/cloude.jpg)';
             }else if(weather1 == 'Rain'){
                 temp_status.innerHTML = '<i class="fa-cloud-rain fa" aria-hidden="true"></i>';
+                body.style.backgroundImage = 'url(images/rain.jpg)';
             }
             else{
                     temp_status.innerHTML = '<i class="fa-cloud fa" aria-hidden="true"></i>';
+                    body.style.backgroundImage = 'url(images/normal.jpg)';
             }
         }catch{
             city_name.innerText = `Please enter a correct city name` ;
